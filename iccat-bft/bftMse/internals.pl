e618bc3aacc2bbdd3b5025d277847a4b379825fa
laurie <laurie@kell.es>
2014-03-03 13:50:14 +0100

    1st commit

---
+++ iccat-bft/bftMse/internals.pl
@@ -0,0 +1,10 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/sub:outline/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
