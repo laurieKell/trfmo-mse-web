44c5edd1530b0d6213c56683e297564da77db0e6
laurie <laurie@kell.es>
2014-03-10 12:28:09 +0100

    2nd commit

---
+++ glossary/internals.pl
@@ -0,0 +1,10 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_rademeyer2007tips/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ glossary/labels.pl
@@ -0,0 +1,17 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_rademeyer2007tips/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+1;
+
